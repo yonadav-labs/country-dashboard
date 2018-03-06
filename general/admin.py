fe6e2aed26b3c942c7a9080b44dec043faa10f48
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2018-03-06 08:35:41 +0530

    implement model

--- general/admin.py
+++ general/admin.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from general.models import *
 
-# Register your models here.
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address_city', 'address_state', 'address_street_1', 'address_street_2', 
+                    'address_zip', 'latitude', 'longitude']
+    search_fields = ['name', 'address_street_1', 'address_street_2', 'address_city', 'address_state', 
+                    'address_zip', 'latitude', 'longitude']
+
+admin.site.register(Address, AddressAdmin)
--- general/models.py
+++ general/models.py
@@ -3,4 +3,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+    name = models.CharField(max_length=255)
+    account_id = models.IntegerField()
+    address_street_1 = models.CharField(max_length=255)
+    address_street_2 = models.CharField(max_length=255)
+    address_city = models.CharField(max_length=255)
+    address_state = models.CharField(max_length=255)
+    address_zip = models.CharField(max_length=255)
+    callback_number_id = models.CharField(max_length=255)
+    location_id = models.IntegerField(null=True)
+    enabled = models.BooleanField(default=True)    
+    search_index = models.TextField(max_length=255, null=True, blank=True)    
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    voiceaxis_id = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'e911_registrations'
